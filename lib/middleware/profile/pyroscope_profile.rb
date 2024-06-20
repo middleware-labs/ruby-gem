7bf17b961ab4a694d19b8e45a2ac00a1da1ff962
arjun-mw <arjun.prajapati@middleware.io>
2024-06-20 16:01:17 +0530

    updated profiling url for v2 support

--- lib/middleware/profile/pyroscope_profile.rb
+++ lib/middleware/profile/pyroscope_profile.rb
@@ -13,11 +13,11 @@ module Middleware
         def self.init
           authentication = Auth::Authentication.new
           return unless authentication.auth
-
+          account = authentication.get_response['data']['account']
           Pyroscope.configure do |config|
             config.application_name = ENV["OTEL_SERVICE_NAME"] || "default-service-name"
-            config.server_address = ENV["MW_PROFILING_SERVER_URL"] || "https://profiling.middleware.io"
-            config.tenant_id = authentication.get_response['data']['account']
+            config.server_address = ENV["MW_PROFILING_SERVER_URL"] || "https://#{account}.middleware.io/profiling"
+            config.tenant_id = account
           end
 
           MwLogger.info("Profiling started")
